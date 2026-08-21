bbcd26001af6713b3dfdc197e585ec3286e6f5e8
Eren61cyber <ahmeterendemirci796@gmail.com>
2026-08-21 04:32:10 +0300

    chore: clean up unused temp scripts, build artifacts and add .gitignore

--- Oyuncu Kariyeri Oyunu/fix2.ps1
+++
@@ -1,7 +0,0 @@
-$gameJs = [IO.File]::ReadAllText("game.js", [System.Text.Encoding]::UTF8)
-
-$newLogic = "        if (this.state.suspendedWeeks > 0) {`r`n            this.state.suspendedWeeks--;`r`n            if (this.state.suspendedWeeks === 0) {`r`n                this.addSocialPost(`"@spor_manset`", `"Spor Manşetleri`", ``Cezası bitti! Kırmızı kart cezası sona eren genç yetenek `${this.state.playerName} yeniden formasına kavuşuyor.``);`r`n            }`r`n        }"
-
-$gameJs = [regex]::Replace($gameJs, '(?s)if \(this\.state\.isSuspended\) \{.*?\}', $newLogic)
-[IO.File]::WriteAllText("game.js", $gameJs, [System.Text.Encoding]::UTF8)
-Write-Host "Fixed."
